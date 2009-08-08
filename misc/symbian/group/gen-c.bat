091dd160655ae4b2b6cda8ff0a020f19351e9fe6
Ivan Kelly <ivan@ivankelly.net>
2009-08-08 22:32:04 +0100

    removing build errors

--- misc/symbian/group/gen-c.bat
+++ misc/symbian/group/gen-c.bat
@@ -1,16 +1,18 @@
 pushd ..\..\..\gsi
-gsc -link _gsi.scm 
-gsc -link _gsilib.scm
+gsc -f -c -check _gsi.scm 
+gsc -f -c -check _gsilib.scm
+gsc -f -link -flat -o _gambcgsi.c _gsi_.c _gsilib_.c
 popd
 
 pushd ..\..\..\lib
-gsc -link _kernel.scm 
-gsc -link _system.scm
-gsc -link _num.scm
-gsc -link _std.scm 
-gsc -link _eval.scm 
-gsc -link _io.scm
-gsc -link _nonstd.scm
-gsc -link _thread.scm 
-gsc -link _repl.scm
+gsc -f -c -check _kernel.scm 
+gsc -f -c -check _system.scm
+gsc -f -c -check _num.scm
+gsc -f -c -check _std.scm 
+gsc -f -c -check _eval.scm 
+gsc -f -c -check _io.scm
+gsc -f -c -check _nonstd.scm
+gsc -f -c -check _thread.scm 
+gsc -f -c -check _repl.scm
+gsc -f -link -flat -o _gambc.c _kernel_.c _system_.c _num_.c _std_.c _eval_.c _io_.c _nonstd_.c _thread_.c _repl_.c
 popd
