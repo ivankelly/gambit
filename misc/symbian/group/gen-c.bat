7377246988d0982ceeb10f4249e96badf3ff9a8f
Ivan Kelly <ivan@ivankelly.net>
2009-08-08 23:10:43 +0100

    fixed c generation

--- misc/symbian/group/gen-c.bat
+++ misc/symbian/group/gen-c.bat
@@ -1,9 +1,3 @@
-pushd ..\..\..\gsi
-gsc -f -c -check _gsi.scm 
-gsc -f -c -check _gsilib.scm
-gsc -f -link -flat -o _gambcgsi.c _gsi_.c _gsilib_.c
-popd
-
 pushd ..\..\..\lib
 gsc -f -c -check _kernel.scm 
 gsc -f -c -check _system.scm
@@ -14,5 +8,12 @@ gsc -f -c -check _io.scm
 gsc -f -c -check _nonstd.scm
 gsc -f -c -check _thread.scm 
 gsc -f -c -check _repl.scm
-gsc -f -link -flat -o _gambc.c _kernel_.c _system_.c _num_.c _std_.c _eval_.c _io_.c _nonstd_.c _thread_.c _repl_.c
+gsc -f -link -flat -o _gambc.c _kernel.c _system.c _num.c _std.c _eval.c _io.c _nonstd.c _thread.c _repl.c
+popd
+
+pushd ..\..\..\gsi
+gsc -f -c -check _gsilib.scm
+gsc -f -link -l ..\lib\_gambc -o _gambcgsi.c _gsilib.c
+gsc -f -c -check _gsi.scm 
+gsc -f -link -l _gambcgsi -o _gsi_.c _gsi.c
 popd
